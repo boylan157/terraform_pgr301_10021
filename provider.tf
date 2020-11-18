9c22e877d05d327862ea87e84bc417f2d32f5e11
Daniel <boylan157@gmail.com>
2020-11-18 03:39:23 +0100

    changed some values in travis

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google-beta" {
-  credentials = file("pgr301-exam-b0bef1aea48c.json")
+  credentials = file("pgr301-exam-ad93aab2bdd9.json")
   project     = "terraform-292215"
   version = "~> 3.0.0-beta.1"
 }
