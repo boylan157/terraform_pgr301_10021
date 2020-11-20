80e0cbe62de3a471d8a075d28b85357081037c9d
Daniel <boylan157@gmail.com>
2020-11-20 15:24:40 +0100

    latest image

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "google_cloud_run_service" "default" {
-  name     = "terraformservicev2"
+  name     = "terraformservice"
   location = "europe-north1"
   project = "pgr301-exam"
 
   template {
     spec {
       containers {
-        image = "eu.gcr.io/pgr301-exam/pgr301_10021/devopsexam@sha256:2daf60761205ea6437dba85d385ed2ad1e899f6394cb60359770c128a2d0ceec"
+        image = "eu.gcr.io/pgr301-exam/pgr301_10021:latest"
       }
     }
   }
