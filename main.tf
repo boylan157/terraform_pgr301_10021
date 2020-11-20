4a1610098989a4588e155b9948c0a66a44b36e22
Daniel <boylan157@gmail.com>
2020-11-20 17:48:26 +0100

    new image

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "google_cloud_run_service" "default" {
   template {
     spec {
       containers {
-        image = "eu.gcr.io/pgr301-exam/pgr301_10021/devopsexam@sha256:4dceeb0f8f668b5351333bfc7f543a1c5500f267db61143738e066c66d3d3fc5"
+        image = "eu.gcr.io/pgr301-exam/pgr301_10021/devopsexam@sha256:4ada9f6825ea53dbcb9a7cce6e2c5978ead2711ffb7d9a3c2cc9cb02d463c447"
         env {
           name = "LOGZ_TOKEN"
           value = var.logz_token
