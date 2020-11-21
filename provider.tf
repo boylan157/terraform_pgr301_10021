ee22da6bd3db3ef9f1e5660233f58c66376ad116
Daniel <boylan157@gmail.com>
2020-11-21 13:14:44 +0100

    added terraform backend

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "gcs" {
+    bucket = "terraform-bucket-pgr301"
+    prefix = "terraformstate"
+    credentials = "pgr301-exam-ad93aab2bdd9.json"
+  }
+}
+
 provider "google-beta" {
   credentials = file("pgr301-exam-ad93aab2bdd9.json")
   project     = "terraform-292215"
